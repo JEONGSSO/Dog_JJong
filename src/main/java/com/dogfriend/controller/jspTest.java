65c0bd7e2400acdf93b8e36af3bfbc0f9a76cbd5
jeongsso <rpsha33@naver.com>
2018-10-10 13:20:09 +0900

    add

--- src/main/java/com/dogfriend/controller/jspTest.java
+++ src/main/java/com/dogfriend/controller/jspTest.java
@@ -33,8 +33,8 @@ public class jspTest {
 	}
 
 	@RequestMapping("/") //requestMapping + mothod get
-	public String jsptest() {
-	return "/test";
+	public String dogHome() {
+	return "/dogHome";
 	}
 
 
