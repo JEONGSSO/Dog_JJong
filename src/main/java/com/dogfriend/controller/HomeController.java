37650b5e3013a784a9457bff48a25b7c9228e372
jeongsso <rpsha33@naver.com>
2018-10-10 18:32:48 +0900

    ts

--- src/main/java/com/dogfriend/controller/HomeController.java
+++ src/main/java/com/dogfriend/controller/HomeController.java
@@ -3,10 +3,10 @@ package com.dogfriend.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller	//controller + reponseBody 객체를 리턴하면 json으로 출력
+@Controller
 public class HomeController {
 
-	@RequestMapping("/") //requestMapping + mothod get
+	@RequestMapping("/")
 	public String dogHome() {
 	return "dogHome";	//webapp/WEB-INF/views/dogHome.jsp실행
 	}
