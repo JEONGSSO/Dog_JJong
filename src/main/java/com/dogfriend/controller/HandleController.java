18218f99720a6ab9b464fcc937ae2970e7c7ce72
JEONGSSO <rpsha33@naver.com>
2018-10-13 01:10:26 +0900

    정수 :: HandleController, HandleVO 수정

--- src/main/java/com/dogfriend/controller/HandleController.java
+++ src/main/java/com/dogfriend/controller/HandleController.java
@@ -18,40 +18,55 @@ public class HandleController{
 
 	HandleVO handle = new HandleVO();
 
-////////////////////////////안드로이드에서 버튼 자동제어 온오프 눌렀을 때////////////////////////////////////
-	@PostMapping("/setpower")	//안드로이드가 자동제어 온오프 누를때 콜해야 할 주소
-	public ResponseEntity<HandleVO> revdAndroid(@RequestBody HandleVO handle) {	//아두이노에서 온 값을 @RequestBody는 자바객체로 만들어 vo담음
+////////////////////////////안드로이드에서 버튼 전원 온오프 눌렀을 때////////////////////////////////////
+	@PostMapping("/setpower")	//안드로이드가 전원 온오프 누를때 콜해야 할 주소
+	public ResponseEntity<HandleVO> setAndroidPower(@RequestBody HandleVO handle) {	//아두이노에서 온 값을 @RequestBody는 자바객체로 만들어 vo담음
 		
-		handle.setPower(handle.isPower());	//power 받아오면 값을 담아준다.
+		handle.setPower(handle.isPower());	//power 받아오면 값을 담아준다. boolean은 isPower == setPower
 		
 		try {
-			return new ResponseEntity<>(handle, HttpStatus.OK);	
+			return new ResponseEntity<>(handle, HttpStatus.OK);		//받은 값 통째로 아두이노 리턴
 		} catch (Exception e) {
 			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 		}
 	}
 
-////////////////////////////안드로이드에서 버튼 눌렀을 때 아두이노로 보내기////////////////////////////////////
-	@GetMapping("/sendarduino")	//온습도 requestMapping + mothod post	//@RequestBody 자바 객체를 만들어준다.
+////////////////////////////아두이노로 전원여부 값 전달////////////////////////////////////
+	@GetMapping("/sendpower")
 	public ResponseEntity<String> sendArduinoTempHumi() {	
 
 		try {
-			return new ResponseEntity<>(handle,HttpStatus.OK);	//성공시 success에 ok를 담는다.
+			return new ResponseEntity<>("Power Send Ok",HttpStatus.OK);
 
 		} catch (Exception e) {
-			e.printStackTrace(); //예외 오류
-			return new ResponseEntity<>("fail",HttpStatus.BAD_REQUEST); //성공시 fail bad 담는다.
+			return new ResponseEntity<>("Power Send Fail",HttpStatus.BAD_REQUEST);
 		}
 	}
 
-	@GetMapping("/ctrl")
-	public Map<Integer,Boolean> autoCotrol() 	//통신규약 정하기
-	{
+	//만약 컨트롤러를 나누면 set, send끼리 나눌지 power, auto 기능끼리 나눌지 상의
 
-		HashMap<Integer, Boolean> hashMap = new HashMap<>();
-		hashMap.put(1, true);
-		hashMap.put(2, false);
-		
-		return hashMap;
+///////////////////////////안드로이드에서 자동모드 클릭했을 때/////////////////////////////////////////////////////
+	@PostMapping("/setautomode")
+	public ResponseEntity<HandleVO> autoMode(@RequestBody HandleVO handle)
+	{
+		handle.setAutoMode(handle.isAutoMode());	//boolean 타입
+
+		try{
+			return new ResponseEntity<>(handle, HttpStatus.OK);
+		}catch(Exception e){
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+	}
+
+///////////////////////////아두이노로 자동모드 값 전달//////////////////////////////////////////////////////////////
+	@GetMapping("/sendauto")
+	public ResponseEntity<String> sendArduinoAutoMode() {	
+
+		try {
+			return new ResponseEntity<>("autoMode OK",HttpStatus.OK);
+
+		} catch (Exception e) {
+			return new ResponseEntity<>("autoMode Fail",HttpStatus.BAD_REQUEST);
+		}
 	}
 }
--- src/main/java/com/dogfriend/domain/HandleVO.java
+++ src/main/java/com/dogfriend/domain/HandleVO.java
@@ -5,4 +5,6 @@ import lombok.Data;
 @Data
 public class HandleVO{
     private boolean isPower ;
+    private boolean autoMode;
+    // private boolean manualMode ; 타입명 상의하기
 }
