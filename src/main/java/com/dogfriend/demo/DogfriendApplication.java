560de5ecd3efca1256b31efd14d3670704e55a14
jeongsso <rpsha33@naver.com>
2018-10-18 00:54:24 +0900

    정수 :: jsp 수정

--- src/main/java/com/dogfriend/demo/DogfriendApplication.java
+++ src/main/java/com/dogfriend/demo/DogfriendApplication.java
@@ -6,9 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
+//@Configuration @EnableConfiguration @ComponentScan 포함하는 어노테이션
 @SpringBootApplication	
+//자동 설정을 담당 exclude는 제외
+// DB 설정을 넣고, 맞는 드라이버와 라이브러리 설치, JDBC 설정을 해야하는데 아직은 비어있으니 제외.
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})	//1008 톰캣 안켜있는데 켜있다할때 넣음 그래도 안되면 componentscan잠시 주석후 껐다가 다시 주석 풀기
-@ComponentScan("com.dogfriend")
+@ComponentScan("com.dogfriend") //com.dogfriend 밑의 패키지들 bean을 자동 등록
 public class DogfriendApplication {
     
 	public static void main(String[] args) {
